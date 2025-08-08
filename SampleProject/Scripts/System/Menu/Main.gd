41f0d93a6365834905324dc1a9c1d95082dcc93f
GianImpr <gianimprota02@gmail.com>
2025-08-08 16:50:12 +0200

    MP bar is now empty if max MP is 0 in the status panel.

--- SampleProject/Scripts/System/Menu/Main.gd
+++ SampleProject/Scripts/System/Menu/Main.gd
@@ -35,7 +35,10 @@ func updateStats():
 		elements.MainStatsValues.text += "%4d" % Global.getStat(stat) + "/" + "%4d" % Global.getStat("M" + stat) + "\n"
 	
 	elements.health_bar.value = float(Global.getStat("HP")) / Global.getStat("MHP") * elements.health_bar.max_value
-	elements.mana_bar.value = float(Global.getStat("MP")) / Global.getStat("MMP") * elements.mana_bar.max_value
+	if Global.player.unlocked_magic:
+		elements.mana_bar.value = float(Global.getStat("MP")) / Global.getStat("MMP") * elements.mana_bar.max_value
+	else:
+		elements.mana_bar.value = 0
 	elements.synergy_bar.value = float(Global.getStat("SP")) / Global.getStat("MSP") * elements.synergy_bar.max_value
 
 	
