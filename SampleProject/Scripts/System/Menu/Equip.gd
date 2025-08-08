442f465176159e62c15fd0c62e846d760dec5520
GianImpr <gianimprota02@gmail.com>
2025-08-08 18:14:51 +0200

    Weapon wheels icons are now updated in the equip menu after loading a save file.

--- SampleProject/Scripts/System/Menu/Equip.gd
+++ SampleProject/Scripts/System/Menu/Equip.gd
@@ -2,7 +2,7 @@ extends MenuState
 class_name InvEquip
 
 @export var labels: Control
-@export var equip_list: GridContainer
+@export var equip_list: EquipMenu
 var accessed_menu: int
 
 func enter():
@@ -11,6 +11,7 @@ func enter():
 	accessed_menu = 0
 	equip_list.player = Global.player.stats
 	updateStats(["ATK", "DEF", "STR", "CON", "INT", "RES", "SYN", "LCK"], labels.SubStatValues)
+	updateQuickWeaponIcons()
 	
 func exit():
 	animation.play("change")
@@ -23,7 +24,13 @@ func Update(delta: float):
 func Physics_Update(delta: float):
 	pass
 	
-func updateStats(stats: Array[String], label: Label):
+func updateStats(stats: Array[String], label: Label) -> void:
 	label.text = ""
 	for stat in stats:
 		label.text += str(Global.player.stats.Stats[stat]) + "\n"
+
+func updateQuickWeaponIcons() -> void:
+	var quick_weapons_icons: Control = equip_list.quick_weapon_icons
+	for i in range(0, EquipMenu.quick_weapons.size()):
+		if EquipMenu.quick_weapons[i] != null:
+			quick_weapons_icons.get_child(i).texture = EquipMenu.quick_weapons[i].icon
