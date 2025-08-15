31dff535609f582607b6c1aa925593335ac0226b
GianImpr <gianimprota02@gmail.com>
2025-08-15 17:36:45 +0200

    Fixed Intro text not appearing if watching it for the second time after resetting.

--- SampleProject/extra_scenes/intro.gd
+++ SampleProject/extra_scenes/intro.gd
@@ -7,6 +7,7 @@ class_name Intro
 var tween: Tween
 const FADE_OUT_DURATION: float = 1
 const SILENT_VOLUME: int = -80
+const INITIAL_TEXT_POSITION: float = 480
 const BASE_FINAL_TEXT_POSITION: float = -20
 const TEXT_DURATION: float = 75
 var text_tween: Tween
@@ -14,6 +15,8 @@ var text_tween: Tween
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	text_tween = get_tree().create_tween()
+	text.position.y = INITIAL_TEXT_POSITION
+	text_tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
 	text_tween.tween_property(text, "position", Vector2(text.position.x, BASE_FINAL_TEXT_POSITION-text.size.y), TEXT_DURATION)
 	
 func _process(delta: float) -> void:
