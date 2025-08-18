03f3b5fa6926404ba4b2ae963759ede8d240262f
GianImpr <gianimprota02@gmail.com>
2025-08-18 21:29:32 +0200

    Added Teleport option in the save room menu.

--- SampleProject/Scripts/System/Menu/save_room_menu.gd
+++ SampleProject/Scripts/System/Menu/save_room_menu.gd
@@ -1,6 +1,13 @@
 extends CanvasLayer
 
 @export var default_button: InventoryButton
+@export var warp_button: InventoryButton
 
 func _ready() -> void:
 	default_button.grab_focus()
+	var can_warp: bool = Global.player.stats.findItem(Skill.Skills.WARP_MEDALLION, Global.player.stats.skill_inventory)
+	warp_button.disabled = not can_warp
+	if can_warp:
+		warp_button.text = "Teleport"
+	else:
+		warp_button.text = "? ? ?"
