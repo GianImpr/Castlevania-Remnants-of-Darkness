461096f2053eb7c565f156fac96be70fa38191c7
GianImpr <gianimprota02@gmail.com>
2025-08-23 12:15:44 +0200

    Implemented Serpent Door which only opens upon collecting the two Serpent Eyes.

--- SampleProject/Maps/Fagaras/snake_door_event.gd
+++ SampleProject/Maps/Fagaras/snake_door_event.gd
@@ -2,12 +2,24 @@ extends StaticBody2D
 class_name SnakeDoorEvent
 
 @export var event_flag: int
+@export var hint_14: HintBoxTrigger
+@export var hint_15: HintBoxTrigger
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if Global.player.stats.event_flags[event_flag]:
 		queue_free()
+	elif not hasEyes():
+		hint_15.queue_free()
+	elif hasEyes():
+		hint_14.queue_free()
+	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if Global.player.stats.findItem(Item.Items.POTION, Global.player.stats.item_inventory):
-		pass
+	if hasEyes():
+		Global.player.stats.event_flags[event_flag] = true
+		queue_free()
+
+func hasEyes() -> bool:
+	return Global.player.stats.findItem(Item.Items.LEFT_SERPENT_EYE+1, Global.player.stats.item_inventory) \
+		   and Global.player.stats.findItem(Item.Items.RIGHT_SERPENT_EYE+1, Global.player.stats.item_inventory)
