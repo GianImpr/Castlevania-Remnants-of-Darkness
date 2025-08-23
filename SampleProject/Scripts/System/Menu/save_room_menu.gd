9b679edc5fc47e735a72fb40b29caf2f3d23ccc3
GianImpr <gianimprota02@gmail.com>
2025-08-23 10:30:04 +0200

    Teleport button now supports localization.

--- SampleProject/Scripts/System/Menu/save_room_menu.gd
+++ SampleProject/Scripts/System/Menu/save_room_menu.gd
@@ -8,6 +8,6 @@ func _ready() -> void:
 	var can_warp: bool = Global.player.stats.findItem(Skill.Skills.WARP_MEDALLION, Global.player.stats.skill_inventory)
 	warp_button.disabled = not can_warp
 	if can_warp:
-		warp_button.text = "Teleport"
+		warp_button.text = tr("TELEPORT_GAME_BUTTON")
 	else:
 		warp_button.text = "? ? ?"
