ce6412df6cc01048830dbe46b83e8fafb4715e3f
GianImpr <gianimprota02@gmail.com>
2025-08-29 15:04:12 +0200

    Changed position of Aguni Laurel's flames.

--- SampleProject/player.gd
+++ SampleProject/player.gd
@@ -139,7 +139,7 @@ func _process(delta: float) -> void:
 		get_tree().create_timer(AGUNI_COOLDOWN).timeout.connect(func(): aguni_on_cooldown = false)
 		var flame = aguni_flames.instantiate()
 		MetSys.get_current_room_instance().add_child(flame)
-		flame.global_position = global_position + Vector2(0,66)
+		flame.global_position = global_position + Vector2(20*facing_position,66)
 		aguni_mp_consumption += delta*AGUNI_LAUREL_COST_PER_SECOND
 		if aguni_mp_consumption >= 1:
 			aguni_mp_consumption = 0
