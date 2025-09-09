296ecb528b8fc6b69924c5d4b214652e28ba51dc
GianImpr <gianimprota02@gmail.com>
2025-09-09 11:31:10 +0200

    EnemySpawner now has to spawn enemies at least 80px away from the player.

--- SampleProject/Scripts/Spawners/enemy_spawner.gd
+++ SampleProject/Scripts/Spawners/enemy_spawner.gd
@@ -4,6 +4,7 @@ class_name EnemySpawner
 @export_range(0.1, 10) var spawn_frequency_in_seconds: float
 @export var min_pos_limits: Vector2
 @export var max_pos_limits: Vector2
+@export var min_distance_from_player: float = 80
 
 func _ready():
 	var timer = Timer.new()
@@ -15,4 +16,9 @@ func _ready():
 func spawn() -> void:
 	var enemy = enemy_to_spawn.instantiate()
 	enemy.position = Vector2(randf_range(min_pos_limits.x, max_pos_limits.x), randf_range(min_pos_limits.y, max_pos_limits.y))
+	if abs(enemy.global_position.x - Global.player.global_position.x) < min_distance_from_player:
+		if (enemy.global_position.x - min_distance_from_player) >= min_pos_limits:
+			enemy.global_position.x -= min_distance_from_player
+		elif (enemy.global_position.x + min_distance_from_player) < min_pos_limits:
+			enemy.global_position.x += min_distance_from_player
 	get_parent().add_child(enemy)
