720226da395d0e5ec2f1521000f4ba73fdb6261f
GianImpr <gianimprota02@gmail.com>
2026-01-18 00:08:06 +0100

    Implemented canvas for training mode.

---
+++ SampleProject/Scripts/Stage_elements/frame.gd
@@ -0,0 +1,18 @@
+extends Node2D
+class_name TrainingFrame
+
+var inside_area: bool = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("up_arrow") and inside_area:
+		Global.training_menu.openMenu()
+		
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	inside_area = true
+	Global.player.tap_up.appear()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	inside_area = false
+	Global.player.tap_up.dismiss()
