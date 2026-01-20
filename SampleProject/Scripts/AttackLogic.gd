d8798561f86e18edac72128f7dfef73b6f6ef9a7
GianImpr <gianimprota02@gmail.com>
2026-01-20 18:44:52 +0100

    Artifact effects on hit now only apply to enemies

--- SampleProject/Scripts/AttackLogic.gd
+++ SampleProject/Scripts/AttackLogic.gd
@@ -141,6 +141,9 @@ func createHitEffect(body: Node2D) -> void:
 	hit_effect.position = Vector2(effect_x, effect_y)
 	Global.player.get_parent().add_child(hit_effect)
 	
+	if body is not Enemy or body is not Zombie:
+		return
+	
 	if Global.player.stats.itemEquipped(Artifact.Artifacts.LITTLE_HAMMER, "artifact") and Global.player.stats.Stats["LCK"] > randi_range(0, 99):
 		var coin = coin_scene.instantiate()
 		coin.global_position = hit_effect.position
