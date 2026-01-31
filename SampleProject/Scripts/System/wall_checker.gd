5ebc01fd3861eb6029eb0ea6ec41576201dcf220
GianImpr <gianimprota02@gmail.com>
2026-01-31 09:49:52 +0100

    Fixed checkOverlap() in WallChecker transforming the origin of the shape to check as well.

--- SampleProject/Scripts/System/wall_checker.gd
+++ SampleProject/Scripts/System/wall_checker.gd
@@ -15,7 +15,8 @@ func checkOverlap() -> bool:
 	
 	var query = PhysicsShapeQueryParameters2D.new()
 	query.shape = parent.collision.shape
-	query.transform = parent.global_transform.scaled(Vector2(0.8, 0.8))
+	query.transform = parent.transform.scaled_local(Vector2(0.8,0.8))
+	#query.transform.origin = Vector2(0,0)
 	
 	query.collision_mask = parent.collision_mask 
 	
