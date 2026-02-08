94c02bb35ccc49a7b4c05db1a63c0409a0e9aa42
GianImpr <gianimprota02@gmail.com>
2026-02-08 21:11:45 +0100

    Added movement and icicle aggro on distance to Frozen Shade.

--- SampleProject/enemies/FrozenShade/FrozenShadeIdle.gd
+++ SampleProject/enemies/FrozenShade/FrozenShadeIdle.gd
@@ -1,19 +1,25 @@
 extends State
 class_name FrozenShadeIdle
 var duration: float
-const MIN_DURATION: float = 0.2
-const MAX_DURATION: float = 1
+const MIN_DURATION: float = 0.5
+const MAX_DURATION: float = 1.2
 const ACTIONS: Array[String] = ["icicle", "freeze"]
 var can_act: bool
+const SPEED: float = 150
+var velocity_tween: Tween
+const CHANGE_VELOCITY_INTERVAL: float = 1.5
+const FREEZE_THRESHOLD: float = 200
 
 func enter():
 	animation.play("idle")
+	applyVelocity()
 	can_act = false
 	duration = randf_range(MIN_DURATION, MAX_DURATION)
 	get_tree().create_timer(duration, false).timeout.connect(func(): can_act = true)
 	
 func exit():
-	pass
+	velocity_tween.kill()
+	player.velocity = Vector2.ZERO
 
 func Update(delta: float):
 	enemy_can_die()
@@ -28,4 +34,14 @@ func Physics_Update(delta: float):
 	pass
 
 func decideAction() -> String:
-	return ACTIONS.pick_random()
+	if absf(Global.player.global_position.x - player.global_position.x) < FREEZE_THRESHOLD:
+		return ACTIONS.pick_random()
+	return "icicle"
+
+func applyVelocity() -> void:
+	velocity_tween = get_tree().create_tween()
+	velocity_tween.set_trans(Tween.TRANS_SINE)
+	velocity_tween.set_ease(Tween.EASE_IN_OUT)
+	velocity_tween.tween_property(player, "velocity", Vector2(SPEED, SPEED) * Vector2(randf_range(-1,1), randf_range(-1,1)), CHANGE_VELOCITY_INTERVAL)
+	if not velocity_tween.finished.is_connected(applyVelocity):
+		velocity_tween.finished.connect(applyVelocity)
