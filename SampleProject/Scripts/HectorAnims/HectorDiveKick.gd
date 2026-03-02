bf85bcb4f7cda5e5a5cdf498909ac1f756d99afe
GianImpr <gianimprota02@gmail.com>
2026-03-02 23:04:30 +0100

    Front dash now only triggered after a diagonal dive kick.

--- SampleProject/Scripts/HectorAnims/HectorDiveKick.gd
+++ SampleProject/Scripts/HectorAnims/HectorDiveKick.gd
@@ -5,6 +5,7 @@ class_name HectorDiveKick
 const HARD_LAND_AFTER_SECONDS: float = 0.2
 var hard_land: bool
 var can_perfect_guard: bool = false
+var diagonal: bool
 
 func enter():
 	hard_land = false
@@ -12,8 +13,10 @@ func enter():
 	get_tree().create_timer(HARD_LAND_AFTER_SECONDS, false).timeout.connect(func(): hard_land = true)
 	if player.direction == 0:
 		animation.play("dive_kick_straight")
+		diagonal = false
 	else:
 		animation.play("dive_kick_diagonal")
+		diagonal = true
 	player.velocity = FALLING_SPEED
 	player.velocity.x *= player.direction
 	sound.play_sound_effect_from_library("dive_kick")
@@ -29,7 +32,7 @@ func Physics_Update(delta: float):
 		if hard_land or player.stats.status[player.stats.Status.POISON] > 0:
 			Transitioned.emit(self, "hard_landing")
 		else:
-			if not Input.is_action_pressed("jump") and player.direction == 0:
+			if not Input.is_action_pressed("jump") and player.direction == 0 and diagonal:
 				Transitioned.emit(self, "front_dash")
 			else:
 				Transitioned.emit(self, "landing")
