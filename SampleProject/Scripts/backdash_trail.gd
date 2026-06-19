73c6ec45576b1bae337a40b62371b80234132f69
GianImpr <gianimprota02@gmail.com>
2026-06-20 01:04:43 +0200

    Updated axe animations and fixed Hector's spritesheet.

--- SampleProject/Scripts/backdash_trail.gd
+++ SampleProject/Scripts/backdash_trail.gd
@@ -3,6 +3,6 @@ extends Sprite2D
 
 func _ready() -> void:
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "modulate", Color(0, 0, 0, 0), duration)
+	tween.tween_property(self, "modulate", Color(0.5,0.5,0.5, 0), duration)
 	await tween.finished
 	queue_free()
