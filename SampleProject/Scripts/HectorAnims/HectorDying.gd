e08dea6df7e8dc5b3bcb66f547d61ad00c9ac488
GianImpr <gianimprota02@gmail.com>
2026-09-17 15:19:21 +0200

    Added new death animation.

--- SampleProject/Scripts/HectorAnims/HectorDying.gd
+++ SampleProject/Scripts/HectorAnims/HectorDying.gd
@@ -4,31 +4,49 @@ class_name HectorDying
 @export var hurtbox: CollisionShape2D
 @export var timer_reset: Timer
 @export var blood: CPUParticles2D
-var can_perfect_guard: bool = false
+const RECOIL_SPEED: Vector2 = Vector2(250, -350)
 const HEIGHT_DECELERATION: float = 0.95
+var can_perfect_guard: bool = false
 const FADE_SCREEN_FADE_IN_COLOR: Color = Color(1,1,1,1)
 const FADE_MUSIC_DURATION: float = 2.5
+var playing_death_anim: bool = false
 
 func _ready() -> void:
 	HectorPetrified.resetGame = _on_reset_timeout
 
 func enter():
+	blood.emitting = false
+	playing_death_anim = false
 	if player.drowning:
 		animation.play("dying_drowning", -1)
 	else:
-		animation.play("dying", -1)
-		voice.play_sound_effect_from_library("Dead")
-	player.velocity = FLOATING_SPEED
-	timer_reset.start()
+		if not player.is_on_floor():
+			animation.play("hurt_air")
+			timer_reset.start()
+			player.velocity.x = RECOIL_SPEED.x * player.facing_position * (-1)
+			player.velocity.y = RECOIL_SPEED.y
+		else:
+			animation.play("dying")
+			voice.play_sound_effect_from_library("Dead")
+			playing_death_anim = true
+			timer_reset.start()
+			player.velocity.x = 0
+			player.velocity.y = 0
 	Global.music_player.fadeMusic(FADE_MUSIC_DURATION)
 	
 func Update(delta: float):
 	pass
 	
 func Physics_Update(delta: float):
-	player.motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
-	player.velocity.y *= HEIGHT_DECELERATION
 	hurtbox.disabled = true
+	
+	if player.is_on_floor() and not playing_death_anim:
+		player.velocity.x = 0
+		player.velocity.y = 0
+		playing_death_anim = true
+		animation.play("dying")
+		voice.play_sound_effect_from_library("Dead")
+		timer_reset.start()
 	
 func _on_reset_timeout() -> void:
 	Global.game_over_screen.showScreen()
--- SampleProject/player.gd
+++ SampleProject/player.gd
@@ -155,6 +155,9 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if (event as InputEventKey).keycode == KEY_0:
 			print(InputHelper.serialize_inputs_for_actions())
+			
+	if event.is_action_pressed("circle"):
+		stats.Stats["HP"] = 0
 	
 func _process(delta: float) -> void:
 	#If harmed, become invulnerable for a while
